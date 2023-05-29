bc8b2aaa2acc12faf11f98edd9cbe0d206de65b8
Sudhanshu shukla <sudhanshu.shukla@outlook.com>
2023-05-29 13:32:13 +0530

    added uuid for products and product details view

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -26,6 +27,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = uuidv4();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -36,5 +38,11 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
