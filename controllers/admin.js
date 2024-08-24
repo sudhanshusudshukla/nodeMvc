08ba75f4883e426d715b6f7bb77d30006046a908
sudhanshu shukla <sudhanshu.shukla@outlook.com>
2024-08-24 21:18:40 +0530

    product edit and update with db changes

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,16 +32,18 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-/* exports.getEditProduct = (req, res, next) => {
+exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  req.user.getProducts({
-    where: { id: prodId },
-  });
-  Product.findByPk(prodId)
+
+  /*   req.user.getProducts({
+      where: { id: prodId },
+    }); */
+  // Product.findByPk(prodId)
+  Product.fetchById(prodId)
     .then((product) => {
       if (!product) {
         return res.redirect("/");
@@ -54,31 +56,33 @@ exports.postAddProduct = (req, res, next) => {
       });
     })
     .catch((err) => console.log(err));
-}; */
+};
 
-/* exports.postEditProduct = (req, res, next) => {
+exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
-    .then((product) => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
-      product.description = updatedDesc;
-      return product.save();
-    })
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedImageUrl,
+    updatedDesc,
+    prodId
+  );
+
+  product.save()
     .then((result) => {
       console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
     });
-}; */
+};
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.fetchAll()
+    /* .getProducts() */
     /*  Product.findAll() */
     .then((products) => {
       res.render("admin/products", {
--- models/product.js
+++ models/product.js
@@ -2,18 +2,26 @@
 const mongodb = require("mongodb");
 const getDb = require("../utils/database").getDb;
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      //upate product
+      dbOp = db.collection('products').updateOne(
+        { _id: this._id }, { $set: this });
+    } else {
+      //insert one
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log("log product this", result);
       })
